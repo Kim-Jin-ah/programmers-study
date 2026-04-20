2364456882c5efbc9d7edcb796758bacc87a7432
Kim-Jin-ah <jina7446@naver.com>
2026-04-20 18:54:42 +0900

    Create 문자열 안에 문자열.py

---
+++ Lv.0/문자열 안에 문자열.py
@@ -0,0 +1,11 @@
+def solution(str1, str2):
+    answer = 0
+    if str2 in str1:
+        return 1
+    else:
+        return 2
+    return answer
+
+## 풀이전략, 핵심 아이디어
+# in 연산자만 알면 쉽게 풀 수 있는 문제
+# find() 로도 풀 수 있어야함. 다음 복습 때 find로 풀어보기
