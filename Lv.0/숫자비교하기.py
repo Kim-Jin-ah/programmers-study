aac7c02e9be0a2f55034a3ebce88d7511126c23d
Kim-Jin-ah <jina7446@naver.com>
2026-05-29 19:09:19 +0900

    Create 숫자비교하기.py

---
+++ Lv.0/숫자비교하기.py
@@ -0,0 +1,12 @@
+def solution(num1, num2):
+
+    if num1 == num2:
+        return 1
+    else:
+        return -1
+
+  ## 풀이전략, 핵심 아이디어
+  # 그냥 간단한 숫자비교
+  # 한 중 if문 구조 사용한다면
+  def solution(num1, num2):
+    return 1 if num1 == num2 else -1
