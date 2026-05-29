408b0847df14de0da854ff6e0af497a0922f7f6e
Kim-Jin-ah <jina7446@naver.com>
2026-05-29 17:26:24 +0900

    Update 옷가게할인받기.py

--- Lv.0/옷가게할인받기.py
+++ Lv.0/옷가게할인받기.py
@@ -6,8 +6,9 @@ def solution(price):
         price *= 0.9
     elif price >= 100000:
         price *= 0.95
-    return price
+    return int(price)
 
 ## 풀이전략, 핵심 아이디어
 # 큰 조건부터 검사해야하는 게 핵심!!
 # 이런 경우엔 큰 범위부터 검사함을 기억하기!!!
+# float가 나올 경우를 대비해 int 값을 리턴하도록 하기 주의!!
